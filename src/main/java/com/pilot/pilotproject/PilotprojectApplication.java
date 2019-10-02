3628e1f3b46e89dd0a4fc2bda3eef4375354ac33
Sumit <sumit.ml@gmail.com>
2019-10-02 21:18:37 +0530

    added project structure

---
+++ src/main/java/com/pilot/pilotproject/PilotprojectApplication.java
@@ -0,0 +1,13 @@
+package com.pilot.pilotproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PilotprojectApplication.class, args);
+    }
+
+}
